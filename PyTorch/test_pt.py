adc390969bbefa35f49bce0721be7141d7a793b6
Alexey Gorodilov <alexeigor@gmail.com>
2023-05-20 12:48:31 +0000

    add batches

--- PyTorch/test_pt.py
+++ PyTorch/test_pt.py
@@ -18,18 +18,18 @@ def benchmark_func(pipe, compiled, prompt):
     n_runs = 10
     latencies = []
     for _ in range(n_runs):
-        start = time.perf_counter()
+        start = time.perf_counter_ns()
         if not compiled:
             with torch.inference_mode():
                 _ = pipe(prompt)
         else:
             _ = pipe(prompt)
         torch.cuda.synchronize()
-        end = time.perf_counter() - start
+        end = time.perf_counter_ns() - start
         latencies.append(end)
     
     time_avg_s = np.average(latencies)
-    return time_avg_s
+    return int(time_avg_s / 1000000.0)
 
 run_compile = True  # Set True / False
 
@@ -37,22 +37,22 @@ def main():
     batch_size = 1
     prompt = ["a photo of an astronaut riding a horse on mars"] * batch_size
 
-    model = "runwayml/stable-diffusion-v1-5"
-    # model = "stabilityai/stable-diffusion-2-1"
-    pipe_base = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.half).to("cuda")
+    # model = "runwayml/stable-diffusion-v1-5"
+    model = "stabilityai/stable-diffusion-2-1"
+    pipe_base = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16).to("cuda")
     if run_compile:
         print("Run torch compile")
         pipe_base.unet = torch.compile(pipe_base.unet, mode="reduce-overhead", fullgraph=True)
 
-    baseline_image = pipe_base(prompt, guidance_scale=7.5).images[0]
-    # for idx, im in enumerate(baseline_image):
-    #    im.save(f"{idx:06}.jpg")
+    baseline_image = pipe_base(prompt, guidance_scale=7.5).images
+    for idx, im in enumerate(baseline_image):
+        im.save(f"{idx:06}.jpg")
 
     prompt = ["a beautiful photograph of Mt. Fuji during cherry blossom"] * batch_size
 
     latency_ms = benchmark_func(pipe_base, run_compile, prompt)
 
-    print(f"Pipeline latency: {latency_ms:.2f}")
+    print("Pipeline latency:", latency_ms, "ms")
 
 if __name__ == "__main__":
     main()
