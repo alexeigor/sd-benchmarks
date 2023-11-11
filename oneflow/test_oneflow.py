efca159fbd69de2cde5fd0a7280abd67cfc25439
Alexey Gorodilov <alexeigor@gmail.com>
2023-11-11 06:47:48 +0000

    update oneflow

--- oneflow/test_oneflow.py
+++ oneflow/test_oneflow.py
@@ -1,25 +1,29 @@
+from onediff.infer_compiler import oneflow_compile
+from onediff.optimization import rewrite_self_attention
+from diffusers import StableDiffusionPipeline
 import oneflow as flow
-flow.mock_torch.enable()
-
-from onediff import OneFlowStableDiffusionPipeline
+import torch
 
 from time import perf_counter
 import numpy as np
+
+sd_args = {"width": 512, "height": 512, "guidance_scale": 7.5, "num_inference_steps": 50}
+
 
 def measure_latency(pipe, prompt):
     # warm up
     # pipe.set_progress_bar_config(disable=True)
     for _ in range(2):
-        _ =  pipe(prompt)
-    flow.cuda.synchronize()
+        _ =  pipe(prompt, **sd_args)
+    flow._oneflow_internal.eager.Sync()
 
     # Timed run
     latencies = []
     for _ in range(10):
         start_time = perf_counter()
         with flow.autocast("cuda"):
-            _ = pipe(prompt)
-        flow.cuda.synchronize()
+            _ = pipe(prompt, **sd_args)
+        flow._oneflow_internal.eager.Sync()
         latency = perf_counter() - start_time
         latencies.append(latency)
     # Compute run statistics
@@ -29,15 +33,23 @@ def measure_latency(pipe, prompt):
     return f"P95 latency (seconds) - {time_p95_s:.2f}; Average latency (seconds) - {time_avg_s:.2f} +\- {time_std_s:.2f};", time_p95_s
 
 def main():
-    pipe = OneFlowStableDiffusionPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5",
+    # model_id = "stabilityai/stable-diffusion-2-1"
+    model_id = "runwayml/stable-diffusion-v1-5"
+    pipe = StableDiffusionPipeline.from_pretrained(
+        model_id,
         revision="fp16",
-        torch_dtype=flow.float16,
-    ).to("cuda")
+        variant="fp16",
+        torch_dtype=torch.float16,
+        safety_checker=None,
+    )
+    pipe = pipe.to("cuda")
+
+    rewrite_self_attention(pipe.unet)
+    pipe.unet = oneflow_compile(pipe.unet)
 
     prompt = "a photo of an astronaut riding a horse on mars"
     with flow.autocast("cuda"):
-        images = pipe(prompt).images
+        images = pipe(prompt, **sd_args).images
         for i, image in enumerate(images):
             image.save(f"{prompt}-of-{i}.png")
 
