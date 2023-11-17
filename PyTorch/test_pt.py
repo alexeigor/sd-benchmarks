9c324bb40c111192e1bdef43c22dcefbbe0995f8
Alexey Gorodilov <alexeigor@gmail.com>
2023-11-17 19:42:37 +0000

    update formating

--- PyTorch/test_pt.py
+++ PyTorch/test_pt.py
@@ -1,5 +1,6 @@
 import os
-os.environ['HF_HOME']='/workspace/.cache/huggingface'
+
+os.environ["HF_HOME"] = "/workspace/.cache/huggingface"
 
 import time
 import numpy as np
@@ -7,17 +8,35 @@ import numpy as np
 import torch
 from diffusers import DiffusionPipeline
 
-sd_args_v15 = {"_model_id_": "runwayml/stable-diffusion-v1-5", "width": 512, "height": 512, "guidance_scale": 7.5, "num_inference_steps": 50}
-sd_args_v21 = {"_model_id_": "stabilityai/stable-diffusion-2-1", "width": 768, "height": 768, "guidance_scale": 7.5, "num_inference_steps": 50}
-sd_args_xl = {"_model_id_": "stabilityai/stable-diffusion-xl-base-1.0", "width": 1024, "height": 1024, "guidance_scale": 7.5, "num_inference_steps": 50}
-
+sd_args_v15 = {
+    "_model_id_": "runwayml/stable-diffusion-v1-5",
+    "width": 512,
+    "height": 512,
+    "guidance_scale": 7.5,
+    "num_inference_steps": 50,
+}
+sd_args_v21 = {
+    "_model_id_": "stabilityai/stable-diffusion-2-1",
+    "width": 768,
+    "height": 768,
+    "guidance_scale": 7.5,
+    "num_inference_steps": 50,
+}
+sd_args_xl = {
+    "_model_id_": "stabilityai/stable-diffusion-xl-base-1.0",
+    "width": 1024,
+    "height": 1024,
+    "guidance_scale": 7.5,
+    "num_inference_steps": 50,
+}
 
 sd_args = sd_args_v15
+
 
 # @torch.inference_mode()
 def benchmark_func(pipe, compiled, prompt):
     for _ in range(5):
-        _ =  pipe(prompt, **sd_args)
+        _ = pipe(prompt, **sd_args)
     # Start benchmark.
     torch.cuda.synchronize()
 
@@ -34,26 +53,30 @@ def benchmark_func(pipe, compiled, prompt):
         torch.cuda.synchronize()
         end = time.perf_counter_ns() - start
         latencies.append(end)
-    
+
     time_avg_s = np.average(latencies)
     return int(time_avg_s / 1000000.0)
 
+
 run_compile = True  # Set True / False
+
 
 def main():
     batch_size = 1
     prompt = ["A majestic lion jumping from a big stone at night"] * batch_size
 
     pipe_base = DiffusionPipeline.from_pretrained(
-            sd_args["_model_id_"],
-            torch_dtype=torch.float16,
-            use_safetensors=True,
-            variant="fp16"
-        ).to("cuda")
-    
+        sd_args["_model_id_"],
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    ).to("cuda")
+
     if run_compile:
         print("Run torch compile")
-        pipe_base.unet = torch.compile(pipe_base.unet, mode="reduce-overhead", fullgraph=True)
+        pipe_base.unet = torch.compile(
+            pipe_base.unet, mode="reduce-overhead", fullgraph=True
+        )
 
     baseline_image = pipe_base(prompt, **sd_args).images
     for idx, im in enumerate(baseline_image):
@@ -65,6 +88,7 @@ def main():
 
     print("Pipeline latency:", latency_ms, "ms")
     print(sd_args)
+
 
 if __name__ == "__main__":
     main()
